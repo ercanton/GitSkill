16e518ab3baee1f12d76963843070c93d49d54bc
ercanton <ercanton@asu.edu>
2024-11-03 20:14:19 -0700

    fixed a bug regarding capitalization and spacing

--- Yoda.java
+++ Yoda.java
@@ -28,9 +28,13 @@ public class Yoda {
         while (input.indexOf(".") != -1)
         {
             int periodLocation = input.indexOf(".");
+            System.out.println(periodLocation);
             String inputPart = input.substring(0, periodLocation);
             System.out.println("InputPart: " + inputPart);
-            input = input.substring(periodLocation + 1, input.length());
+            if (periodLocation == input.length() -1)
+                input = input.substring(periodLocation + 1, input.length());
+            else
+                input = input.substring(periodLocation + 2, input.length());
             System.out.println("Input: " + input);
             String yodafied = inputPart;
             for (String phrase : yodaPhrases)
@@ -38,9 +42,10 @@ public class Yoda {
                 if (inputPart.indexOf(phrase) != -1)
                 {
                     int phraseLength = phrase.length();
-                    System.out.println("found a phrase!");
+                    //System.out.println("found a phrase!");
                     System.out.println(inputPart.substring(phraseLength + 1, inputPart.length()));
-                    yodafied = inputPart.substring(phraseLength + 1, inputPart.length()) + " " + phrase + ".";
+                    String firstPart = inputPart.substring(0, inputPart.indexOf(phrase));
+                    yodafied = firstPart + inputPart.substring(phraseLength + 1, inputPart.length()) + " " + phrase + ". ";
                     yodafied = yodafied.substring(0, 1).toUpperCase() + yodafied.substring(1);
                 }
             }
